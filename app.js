20432c510aaa84b7e79cac68b0777cc1a720acae
Psyon <143930298+hailsayan@users.noreply.github.com>
2024-01-04 13:41:01 +0330

    update filePath

--- app.js
+++ app.js
@@ -7,14 +7,9 @@ const port = 3000;
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-app.use(express.static('public'));
-app.use(express.static('images'));
-app.use(express.static('files'));
-
-
-app.use('/static', express.static('public'));
-app.use('/images', express.static('images'));
-
+const path = require('path');
+app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/', homeRouter);
 app.use('/users', usersRouter);
