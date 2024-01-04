30693f9a01920bf4dd6647aedcfb76096fba8b91
Psyon <143930298+hailsayan@users.noreply.github.com>
2024-01-04 13:29:40 +0330

    makeFilepath

--- app.js
+++ app.js
@@ -5,7 +5,16 @@ const app = express();
 const port = 3000;
 
 app.set('view engine', 'pug');
-app.set('views', './views')
+app.set('views', './views');
+
+app.use(express.static('public'));
+app.use(express.static('images'));
+app.use(express.static('files'));
+
+
+app.use('/static', express.static('public'));
+app.use('/images', express.static('images'));
+
 
 app.use('/', homeRouter);
 app.use('/users', usersRouter);
