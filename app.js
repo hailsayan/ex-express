ca96527d495699ba4a58956b5e0992e08ff7bc5e
Psyon <143930298+hailsayan@users.noreply.github.com>
2024-01-04 15:20:44 +0330

    bodyParser

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require ('express');
 const homeRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 3000;
 
 app.set('view engine', 'pug');
 app.set('views', './views');
+
+app.use(bodyParser.urlencoded(extended: true));
 
 const path = require('path');
 app.use('/static', express.static(path.join(__dirname, 'public')));
