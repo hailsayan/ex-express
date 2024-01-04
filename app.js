7ad013d715e7704701a87eca2fada90701d24ef8
Psyon <143930298+hailsayan@users.noreply.github.com>
2024-01-04 11:47:20 +0330

    update app

--- app.js
+++ app.js
@@ -4,8 +4,25 @@ const usersRouter = require('./routes/users');
 const app = express();
 const port = 3000;
 
-app.use('/', homeRouter);
-app.use('/users', usersRouter);
+// app.use('/', homeRouter);
+// app.use('/users', usersRouter);
+
+const myLogger = function (req, res, next){
+    req.activeTime = (new Date()).toLocaleTimeString()
+    next()
+}
+
+app.use(myLogger);
+
+app.get('/', (req, res)=>{
+    console.log('home sweet home')
+    res.send(`home page - last seen recently: ${req.activeTime}`)
+})
+
+app.get('/about', (req, res)=>{
+    console.log('about page')
+    res.send(`about page - last seen recently: ${req.activeTime}`)
+})
 
 app.listen(port, ()=>{
     console.log(`app is running on ${port}`);
